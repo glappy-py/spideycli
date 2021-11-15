2c2fb106e260da6c9bd0c4b5d1f1c573808f0d51
GLAPPY <satwikv7@gmail.com>
2021-11-15 21:49:41 +0530

    added ASCII spidey logo

--- bin/spidey.dart
+++ bin/spidey.dart
@@ -3,18 +3,18 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 
 void main(List<String> args) {
+  // Use this for development test
+  String currentVersion = File("version.txt").readAsLinesSync()[0];
+
+  // Use this for production build
+  // String currentVersion =
+  //     File(Platform.resolvedExecutable.split("spidey.exe")[0] + '/version.txt')
+  //         .readAsLinesSync()[0];
   http
       .get(Uri.https("raw.githubusercontent.com",
           "/glappy-py/SpideyCLI-BUILD-/main/version.txt"))
       .then((response) async {
     String latestVersion = response.body.split("\n")[0];
-    // Use this for development test
-    // String currentVersion = File("version.txt").readAsLinesSync()[0];
-
-    // Use this for production build
-    String currentVersion = File(
-            Platform.resolvedExecutable.split("spidey.exe")[0] + '/version.txt')
-        .readAsLinesSync()[0];
     if (latestVersion != currentVersion) {
       print(
           'SpideyCLI update available : v$latestVersion . You are currently using $currentVersion . run \'spideyupdate\' to update SpideyCLI to the latest version');
@@ -24,15 +24,39 @@ void main(List<String> args) {
     if (args[0] == "help") {
       help();
     } else if (args[0] == "-v") {
-      // For development test
-      // print(File("version.txt").readAsStringSync().split("\n")[0]);
-      // For production build
-      print(File(Platform.resolvedExecutable.split("spidey.exe")[0] +
-              '/version.txt')
-          .readAsLinesSync()[0]);
+      print(currentVersion);
+    } else if (args[0] == "logo") {
+      print("""
+
+MMMMMMMMMMMMWNWNXWMMMMMMMMMMMMMMNXWNNWMMMMMMMMMMMM
+MMMMMMMMMMMXk0XkOWMMMMMMMMMMMMMMNkOXOOWMMMMMMMMMMM
+MMMMMMMMMMXoxXdoXMMMMMMMMMMMMMMMMOlOKoxNMMMMMMMMMM
+MMMMMMMMMNllKk;xWMMMMMMMMMMMMMMMMNl:KO:xWMMMMMMMMM
+MMMMMMMMWx;kK;,0MMMMMMMMMMMMMMMMMMx.oXo;0MMMMMMMMM
+MMMMMMMMK;;Kd.:XMMWWWMMMMMMMMWWMMMO.'0k'lNMMMMMMMM
+MMMMMMMWd.cXx.'ldoolokO0K0OOdlooodc.,00,'OMMMMMMMM
+MMMMMMMK; cKOo::;,,;.. .... .,;,;:::dKO, lWMMMMMMM
+MMMMMMMXo..:coddc;;,.        .;;;lxdlc,.'xWMMMMMMM
+MMMMMMMMWKOXWXx:;ll,.       ..;lc;lONNK0XWMMMMMMMM
+MMMMMMMMMMW0o:cxd;'co.      ;o;'cxd:cxKWMMMMMMMMMM
+MMMMMMWNKdc:o0WWl '00'      :Xx..kMNkl:lkXNWMMMMMM
+MMMMMNo'':kXWMMNc ,KNc     .xWx..xMMMWKd;';kWMMMMM
+MMMMMNc :XMMMMMNc ;KMO.    ;XMk..xMMMMMMO'.xMMMMMM
+MMMMMWd.:NMMMMMWl ;XMNo   .kMMO..kMMMMMM0'.OMMMMMM
+MMMMMMO.,KMMMMMWo ;XMMXo:cxWMMO..OMMMMMMk.;XMMMMMM
+MMMMMMX:'OMMMMMMx.;XMMMWWWMMMMO.'0MMMMMWo.dWMMMMMM
+MMMMMMMk,dWMMMMM0',KMMMMMMMMMMk.:NMMMMMX:;KMMMMMMM
+MMMMMMMNocKMMMMMNc,0MMMMMMMMMMx'dWMMMMMk:kMMMMMMMM
+MMMMMMMMXoxWMMMMMx;kMMMMMMMMMWl;KMMMMMXoxWMMMMMMMM
+MMMMMMMMMKxKMMMMMXcoWMMMMMMMMKcdWMMMMWOkNMMMMMMMMM
+MMMMMMMMMMXXWMMMMMOl0MMMMMMMWxlKMMMMMNKNMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMWkkNMMMMMMKx0MMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMNKXWMMMMWKKWMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMWWWMMMMWWMMMMMMMMMMMMMMMMMMMMM""");
     } else {
       print("");
-      print("use spidey help to get a list of valid commands");
+      print(
+          "That command does not exist. Use spidey help to get a list of valid commands");
     }
   } catch (e) {
     print("");
@@ -41,12 +65,13 @@ void main(List<String> args) {
 }
 
 void help() {
-  print(
-      """
+  print("""
   
   spidey help  -  display this help panel
 
   spideyupdate  -  update SpideyCLI. NOTE : there's no space between spidey and update (it's 'spideyupdate')
+
+  spidey logo  -  display the spidey logo. That's it
 
   spidey -v  -  show the version of SpideyCLI you are currently using
 
